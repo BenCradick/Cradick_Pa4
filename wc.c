c76a56bb40d1036b955bf8906d795f2ee6c7e73c
Ben Cradick <ben.cradick@gmail.com>
2017-11-13 18:37:38 -0600

    All functions done for wc.c

--- wc.c
+++ wc.c
@@ -1,5 +1,16 @@
+/* Ben Cradick
+ * cs2750 PA4
+ * 11-12-2017
+ */
+
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 #include <ctype.h>
+int numChar(FILE*);
+int numLines(FILE*);
+int numWords(FILE*);
+
 struct fileStats{
     int wordCount;
     int charCount;
@@ -16,10 +27,26 @@ int main(int argc, char *argv[]) {
     //declared outside of loop to keep program backwards compatible to C90
     size_t i;
     for(i = 1; i < argc; i++){
+        //setting FILE pointer to the first document and setting to "read"
+        FILE* f = fopen(argv[i], "r");
 
-        FILE *f = fopen(argv[i], "r");
-        printf("%s", argv[i] + '\n');
-        printf("%s", "\n");
+        //Reallocating memory for the new string that is going to be the file holder for later.
+        memset(fileStat[i].fileName, '\0', sizeof((char)argv[i]));
+        strcpy(fileStat[i].fileName, argv[i]);
+
+        //TODO remove this testing command
+
+        //Assigning all the values allowed by the struct
+        fileStat[i].wordCount = numWords(f);
+        fileStat[i].lineCount = numLines(f);
+        fileStat[i].charCount = numChar(f);
+
+
+
+
+
+
+
     }
 
     return 0;
@@ -27,29 +54,63 @@ int main(int argc, char *argv[]) {
 // Functions solely exist in this script to keep the code modular and easier to maintain.
 
 /* numChar() counts the number of characters in any given file
- *
- * TODO: describe how this function works.
- *
+ * Takes in a file
+ * loops through every character in the file with fgetc(f)
+ * if the character is End Of File it breaks loops and returns the total number of characts.
  */
-int numChar(){
-
-    return 0;
+int numChar(FILE *f){
+    int count = 0;
+    int i;
+    while((i = fgetc(f))) {
+        if (i == EOF) {
+            rewind(f);
+            return count;
+        }
+        count++;
+    }
+return 0;
 
 }
 /*
  * numLines() gives the number of lines in any given file make changes
+ * Takes in a file increments an integer initialized to zero if a space tab or newline are the current character in the file
  *
- * TODO: describe how function works
  */
-int numLines(){
+int numLines(FILE *f){
+    int count = 0;
+    int i;
+    while((i = fgetc(f))) {
+        if (i == EOF) {
+            count++;
+            rewind(f);
+            return count;
+        }
+        else if(i == '\n')
+        {
+            count++;
+        }
 
+    }
     return 0;
 }
 /*
  * numWords() gives the number of words in any given file
  * TODO: describe how function works
  */
-int numWords(){
+int numWords(FILE *f){
+    int count = 0;
+    int i;
+    while((i = fgetc(f))) {
+        if (i == EOF) {
+            count++;
+            rewind(f);
+            return count;
+        }
+        else if(i == '\n' || i == '\t' || i == ' ')
+        {
+            count++;
+        }
 
+    }
     return 0;
 }
